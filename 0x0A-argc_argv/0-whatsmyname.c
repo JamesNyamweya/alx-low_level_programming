8420b72fa6ad83156bb819456053948501dab5d5
James Nyamweya <nyamweyaj@gmail.com>
2023-06-13 13:25:37 -0700

    Function Prototypes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Returns 0 if successful
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
